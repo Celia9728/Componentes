435a914ff5a57103c383cfa7702ed6f71daec99a
Celia <cml0034@alu.medac.es>
2026-01-29 23:16:25 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   cards/card3/style.css
    	new file:   pruebas/prueba2.css
    	new file:   pruebas/prueba2.html
    	new file:   pruebas/prueba2.js
    	new file:   pruebas/pruebas.html
    	new file:   pruebas/pruebas.js

---
+++ pruebas/pruebas.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  
+  document.addEventListener("mousemove", function (e) {
+    let circulo = document.querySelectorAll("div");
+    console.log(`x: ${e.clientX}, y: ${e.clientY}`);
+
+    for (let i = 0; i < circulo.length; i++) {
+      circulo[i].style.left = e.clientX + "px";
+      circulo[i].style.top = e.clientY + "px";
+    }
+  });
+
+  document.addEventListener("click", function (e) {
+    let nuevoDiv = document.createElement("div");
+    nuevoDiv.setAttribute("class", "nuevoDiv");
+    nuevoDiv.style.left = e.clientX + "px";
+    nuevoDiv.style.top = e.clientY + "px";
+
+    document.body.appendChild(nuevoDiv);
+
+    // setTimeout(() => {
+    //   nuevoDiv.setAttribute("class", "expand");
+    // }, 10);
+  }); 
+};
