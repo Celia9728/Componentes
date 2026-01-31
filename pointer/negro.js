fb755b1a719fbfbd81320977f5a0a5b3577d9213
Celia <cml0034@alu.medac.es>
2026-01-31 16:41:50 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   pointer/0075679886606.webp
    	new file:   pointer/OIP.jpg
    	modified:   pointer/negro.css
    	modified:   pointer/negro.html
    	modified:   pointer/negro.js
    	modified:   pointer/prueba2.html

--- pointer/negro.js
+++ pointer/negro.js
@@ -1,11 +1,22 @@
 window.onload = function () {
-      document.addEventListener("mousemove", function (e) {
-    let circulo = document.querySelectorAll("div");
+  let linterna = document.querySelector("#linterna");
+  let fondo = document.querySelector(".fondoIluminado");
+  document.addEventListener("mousemove", function (e) {
+    linterna.style.left = e.clientX + "px";
+    linterna.style.top = e.clientY + "px";
 
-    for (let i = 0; i < circulo.length; i++) {
-      circulo[i].style.left = e.clientX + "px";
-      circulo[i].style.top = e.clientY + "px";
-    }
+    fondo.style.setProperty("--x", e.clientX + "px");
+    fondo.style.setProperty("--y", e.clientY + "px");
   });
 
-}
+  linterna.addEventListener("click", function (params) {
+    console.log(linterna.getAttribute("class"));
+    if (this.getAttribute("class") == "encendida") {
+      this.setAttribute("class", "apagada");
+      fondo.setAttribute("class", "fondoNegro");
+    } else {
+      this.setAttribute("class", "encendida");
+      fondo.setAttribute("class", "fondoIluminado");
+    }
+  });
+};
