6e37799890d2cc20659301c9d4606021be5ab058
Celia <cml0034@alu.medac.es>
2026-02-03 12:10:10 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   nombre/style.css
    	new file:   ojos/index.html
    	new file:   ojos/script.js
    	new file:   ojos/style.css
    	deleted:    pointer/OIP.jpg
    	renamed:    pointer/prueba2.css -> pointer/bolitas/prueba2.css
    	renamed:    pointer/prueba2.html -> pointer/bolitas/prueba2.html
    	renamed:    pointer/pruebas.html -> pointer/bolitas/pruebas.html
    	renamed:    pointer/pruebas.js -> pointer/bolitas/pruebas.js
    	renamed:    pointer/0075679886606.webp -> pointer/linterna/0075679886606.webp
    	renamed:    pointer/negro.css -> pointer/linterna/negro.css
    	renamed:    pointer/negro.html -> pointer/linterna/negro.html
    	renamed:    pointer/negro.js -> pointer/linterna/negro.js

---
+++ ojos/script.js
@@ -0,0 +1,10 @@
+window.onload = function (evento) {
+  let pupilas = document.querySelectorAll(".pupila");
+
+  document.addEventListener("mousemove", function (evento) {
+    console.log(evento.clientX);
+    console.log(evento.clientY);
+
+    
+  });
+};
