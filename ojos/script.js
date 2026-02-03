7c2451a949f900a76db6e5d6145c149fe042d44b
Celia <cml0034@alu.medac.es>
2026-02-03 20:23:05 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   nombre/style.css
    	modified:   ojos/script.js
    	modified:   ojos/style.css

--- ojos/script.js
+++ ojos/script.js
@@ -1,10 +1,45 @@
-window.onload = function (evento) {
+window.onload = function () {
   let pupilas = document.querySelectorAll(".pupila");
 
   document.addEventListener("mousemove", function (evento) {
-    console.log(evento.clientX);
-    console.log(evento.clientY);
+    // console.log(evento.clientX);
+    // console.log(evento.clientY);
 
-    
+    for (let i = 0; i < pupilas.length; i++) {
+      pupilas[i].style.left = evento.clientX + "px";
+      pupilas[i].style.top = evento.clientY + "px";
+    }
+
+    // console.log(pupilas[0].style.left);
+    // console.log(pupilas[0].style.top);
   });
+
+  let cuencas = document.querySelectorAll(".cuenca");
+
+  const coordenadas = [];
+
+  for (let i = 0; i < cuencas.length; i++) {
+    const rect = cuencas[i].getBoundingClientRect(); // Saca los datos de la pantalla
+
+    coordenadas.push(rect);
+  }
+
+  console.log(coordenadas);
+
+  /*const left = [];
+  const right = [];
+  const top = [];
+  const bottom = [];
+
+  for (let i = 0; i < cuencas.length; i++) {
+    let estilos = window.getComputedStyle(cuencas[i]);
+
+    left[i] = estilos.getPropertyValue("left");
+    right[i] = estilos.getPropertyValue("right");
+    top[i] = estilos.getPropertyValue("top");
+    bottom[i] = estilos.getPropertyValue("bottom");
+  }
+
+  console.log(left, right, top, bottom);
+  console.log(cuencas[0].style.left);*/
 };
